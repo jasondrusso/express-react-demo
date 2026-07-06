67264350f010c5d48ddf65866bededd25b7f6f41
Jason Russo <jdrusso@juno.com>
2026-07-06 16:47:41 -0400

    Add and configure Jasmine Testing Framework to run unit tests.

---
+++ server/spec/test1Spec.mjs
@@ -0,0 +1,5 @@
+describe("A suite", function() {
+    it("contains a space with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
---
+++ server/src/db/mysqldbSpec.js
@@ -0,0 +1,5 @@
+describe("MySql test suite", function() {
+    it("", function() {
+        expect(true).toBe(true);
+    });
+});
